e99a74bde79a4871a80fded5a7fe9d0aef14da37
Square <aayushk4980@gmail.com>
2024-07-26 15:52:14 +0530

    Added code block regex

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -19,7 +19,8 @@ export default function Preview() {
     const linkReg = useReg(/\[(.*?)\]\((.*?)\)/g);    // Regex for links.
     const imgReg = useReg(/!\[(.*?)\]\((.*?)\)/g);    // Regex for images.
     const hrReg = useReg(/^(-{3})/gm);    // Regex for horizontal ruler.
-    const inLinkCodeReg = useReg(/`([^`]*)`/g);     // Regex for inline code.
+    const inLineCodeReg = useReg(/`([^`]*)`/g);     // Regex for inline code.
+    const codeBlockReg = useReg(/```([\s\S]*?)```/g);   // Regex for code block;
 
     // This function converts the text in the markdown format and returns it.
     const convertToMarkdown = useCallback(() => {
@@ -42,9 +43,9 @@ export default function Preview() {
         };
 
         // Heading formatting.
-        result = result.replace(headingReg, (match, p1) => {
+        result = result.replace(headingReg, (p1, p2) => {
             const level = p1.length;
-            const content = match.slice(level + 1).trim();
+            const content = p2.trim();
             return `<h${level} class="${headingSize[level]} ml-1 mt-5 mb-4 font-bold">${content}</h${level}>`
         });
 
@@ -64,17 +65,20 @@ export default function Preview() {
         result = result.replace(imgReg, '<img class="max-w-full" src="$2" alt="$1" />');
 
         // Link formatting.
-        result = result.replace(linkReg, '<a class="text-sky-400 hover:underline underline-offset-2" href="$2" target="_blank" rel="noopener noreferrer">$1</a>')
+        result = result.replace(linkReg, '<a class="text-sky-400 hover:underline underline-offset-2" href="$2" target="_blank" rel="noopener noreferrer">$1</a>');
+
+        // Code Block formatting.
+        result = result.replace(codeBlockReg, '<pre class="w-full bg-slate-800 p-2 font-sans rounded">$1</pre>');
 
         // Inline code formatting.
-        result = result.replace(inLinkCodeReg, '<span class="text-slate-200 bg-slate-800 px-2 py-[2px] rounded-full">$1</span>');
+        result = result.replace(inLineCodeReg, '<span class="text-slate-200 bg-slate-800 px-2 py-[2px] rounded-full">$1</span>');
         
         // Horizontal ruler formatting.
         result = result.replace(hrReg, '<hr />');
 
         // Updating markDown state.
         setMarkDown(result);
-    }, [text, setMarkDown]);
+    }, [text, setMarkDown, headingReg, unorderedListReg, boldReg, italicReg, strikeThroughReg, linkReg, imgReg, hrReg, inLineCodeReg, codeBlockReg, setMarkDown]);
 
     // Convert the text into markdown when actual text changes.
     useEffect(() => {
